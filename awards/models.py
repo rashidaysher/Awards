3c5540406ab068c5c5ea92831cce3824dacafd06
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-10-28 18:07:02 +0300

    defining the decorator for getting the ratings

--- awards/models.py
+++ awards/models.py
@@ -42,4 +42,37 @@ class Project(models.Model):
     def __str__(self):
         return self.name            
 
-          
+class Rating(models.Model):
+    rating=(
+        (1, '1'),
+        (2, '2'),
+        (3, '3'),
+        (4, '4'),
+        (5, '5'),
+        (6, '6'),
+        (7, '7'),
+        (8, '8'),
+        (9, '9'),
+        (10, '10'),
+    )          
+    
+    design = models.IntegerField(choices=rating, default=0, blank=True)
+    usability = models.IntegerField(choices=rating, blank=True)
+    content = models.IntegerField(choices=rating, blank=True)
+    score = models.FloatField(default=0, blank=True)
+    design_average = models.FloatField(default=0, blank=True)
+    usability_average = models.FloatField(default=0, blank=True)
+    content_average = models.FloatField(default=0, blank=True)
+    user = models.ForeignKey(User, on_delete=models.CASCADE, null=True, related_name='rater')
+    post = models.ForeignKey(Project, on_delete=models.CASCADE, related_name='ratings', null=True)
+
+    def save_rating(self):
+        self.save()
+
+    @classmethod
+    def get_ratings(cls, id):
+        ratings = Rating.objects.filter(project_id=id).all()   
+        return ratings
+
+    def __str__(self):
+        return f'{self.project} Rating'  
