f63109870edde2be1820238c7045fe55c11057f9
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-10-28 13:07:39 +0300

    add variables to the profile class

--- awards/models.py
+++ awards/models.py
@@ -5,3 +5,8 @@ from django.core.validators import MinLengthValidator
 # Create your models here.
 class Profile(models.Model):
     profile_pic = models.ImageField(upload_to = 'profilepic/', default = 'default.jpeg')
+    user = models.OneToOneField(User, on_delete=models.CASCADE, null=True)
+    name = models.CharField(max_length=50, blank=True)
+    bio = models.CharField(max_length= 300)
+    email = models.EmailField()
+    
