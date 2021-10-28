bcc09be84978c015b780d65d15b7fbaa4346c56d
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-10-28 13:04:30 +0300

    add profile class to define profile

--- awards/models.py
+++ awards/models.py
@@ -1,3 +1,7 @@
 from django.db import models
+from django.contrib.auth.models import User
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+class Profile(models.Model):
+    profile_pic = models.ImageField(upload_to = 'profilepic/', default = 'default.jpeg')
