6e3592e27112989d9a6b7d2cbbc4d81fe8ef1ad6
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-10-29 14:54:32 +0300

    modified the view function

--- awards/views.py
+++ awards/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpResponseRedirect
+from django.urls import reverse
+from django.contrib.auth.decorators import login_required
+from .models import Profile,Project,Rating
+from .forms import NewProjectForm,ProfileUpdateForm,RatingsForm
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate
+from rest_framework import status
+from rest_framework.response import Response
+from django.http import JsonResponse
+
+from rest_framework.views import APIView
+from .serializer import ProfileSerializer,ProjectSerializer
+
 
 # Create your views here.
