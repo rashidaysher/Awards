6e0dd02b95794e3d373a3c187ef6b8186f69609b
rashidaysher <rashidaisha.ahmed96@gmail.com>
2021-10-29 17:07:22 +0300

    defining registrstion fields

---
+++ awards/forms.py
@@ -0,0 +1,7 @@
+from django import forms
+from .models import Profile, Project
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm
+from .models import Project,Profile,Rating
+
+
